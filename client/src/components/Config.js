651a3b7b7a783be2770bc7cb651179202fa66419
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-15 23:00:42 +0530

    added api integration

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_KEY = "2d3f6e02fe709707763b4b0e608679ae";
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const IMAGE_URL = "http://image.tmdb.org/t/p/";
